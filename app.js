3f3ca811d65dac8b45f76150f2f1181623b36934
Nipurn Bhaal <nbhaal0805@gmail.com>
2023-08-26 04:42:32 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import router from "./routes/user.js";
+import { config } from "dotenv";
+config({
+  path: "./data/config.env",
+});
+
+export const app = express();
+
+//Using Middleware
+app.use(express.json());
+app.use("/users", router);
+app.get("/", (req, res) => {
+  res.send("Nice Working");
+});
